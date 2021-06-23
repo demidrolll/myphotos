d2ca9342e05cc6fadb5b51d9289377006029859d
ida <demi-drolll@yandex.ru>
2021-06-23 09:36:10 +0300

    deploy

---
+++ myphotos-ejb/src/main/java/com/demidrolll/NewSessionBean.java
@@ -0,0 +1,12 @@
+package com.demidrolll;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    public void businessMethod() {
+    }
+}
